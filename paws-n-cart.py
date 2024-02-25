117559fe80eab5f32afafe5ed396b36cf9ce0640
Amil Bello <belloamil8@gmail.com>
2024-02-25 15:34:11 +0000

    backend 'cart add' function. frontend 'cart add' function style(table form)

--- paws-n-cart.py
+++ paws-n-cart.py
@@ -1,7 +1,19 @@
 from shop_items import shop_items
 
+from tabulate import tabulate
+
 
 # ---------------------------------- Backend --------------------------------- #
+cart = {
+    "items": [],
+    "total": 0,
+}
+
+
+def _add_to_cart(item):
+    cart["total"] += item["price"]
+    cart["items"].append(item)
+    return cart
 
 
 # --------------------------------- Frontend --------------------------------- #
@@ -11,13 +23,42 @@ def add_item_to_cart():
     for id, item in shop_items.items():
         print(f"{id}) {item['name']} - {item['price']}")
 
+    while True:
+        user_option = input("Please select the item you'd like to add to cart:\n")
+        if user_option in shop_items.keys():
+            _add_to_cart(shop_items[user_option])
+            view_cart()
+
+        else:
+            print("please choose a valid option")
+
 
 def remove_cart_item():
     print("remove item")
 
 
 def view_cart():
-    print("viewing cart")
+    cart_data = {}
+    cart_table = [["item", "count", "unit price", "subtotal"]]
+    cart_total = round(cart["total"], 2)
+
+    # Combine common items and show count
+    for item in cart["items"]:
+        if item["id"] not in cart_data:
+            cart_data[item["id"]] = {**item, "count": 1}
+        else:
+            cart_data[item["id"]]["count"] += 1
+
+    # Only show name, unit price and total price
+    for id, item in cart_data.items():
+        item_subtotal = item["count"] * item["price"]
+        cart_table.append([item["name"], item["count"], item["price"], item_subtotal])
+
+    # Printing result
+    print("\n  Current Cart")
+    print(tabulate(cart_table, headers="firstrow"))
+    print(f"Cart Total: {cart_total} - total items: {len(cart['items'])}")
+    print("\n")
 
 
 site_functions = {
--- shop_items.py
+++ shop_items.py
@@ -1,6 +1,6 @@
 shop_items = {
-    "1": {"name": "Bark treat", "price": 4.99},
-    "2": {"name": "Whiskers", "price": 14.99},
-    "3": {"name": "Rabbit Snack", "price": 10.99},
-    "4": {"name": "fish nibble", "price": 7.50},
+    "1": {"id": "1", "name": "Bark treat", "price": 4.99},
+    "2": {"id": "2", "name": "Whiskers", "price": 14.99},
+    "3": {"id": "3", "name": "Rabbit Snack", "price": 10.99},
+    "4": {"id": "4", "name": "fish nibble", "price": 7.50},
 }
